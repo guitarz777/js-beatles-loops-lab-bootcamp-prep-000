721d1b893f16ceb9b6138ee299383e496040bccb
guitarz777@gmail.com <guitarz777@gmail.com>
2018-09-04 00:18:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// add solution here
+
+function theBeatlesPlay (musicians, instruments){
+  var sentence = []
+  for (let i=0; i < musicians.length; i++){
+    sentence.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return sentence
+}
+
+
+var i = 0
+function johnLennonFacts(facts){
+  while (i < facts.length){
+  var string = facts[i]
+  string += "!!!"
+  facts.push(string);
+  i = i+1;
+  }
+  return facts
+}
